a1a747f35d180eb3bc5ae512710253f7fd19a8da
7Spade <7s.i@pm.me>
2026-03-18 21:10:47 +0800

    1

--- core/knowledge-core/index.ts
+++ core/knowledge-core/index.ts
@@ -4,12 +4,3 @@
  * Purpose: Integration facade that re-exports module contracts and local scaffolds.
  * Dependency Direction: interfaces -> application -> domain <- infrastructure
  */
-
-// Canonical runtime module exports can be re-enabled when module aliases are available.
-// export * from '@/modules/knowledge'
-// export * from '@/modules/taxonomy'
-// export * from '@/modules/retrieval'
-
-// Local scaffold exports for guided implementation in core/knowledge-core.
-export * from './domain/entities/knowledge.entity'
-export * from './application/use-cases/create-knowledge'
