aaaeddf44e428c85539b4f77c63ab10ba917fe5c
122sp7 <122sp7@gmail.com>
2026-03-20 23:55:51 +0800

    删除 index.ts

--- core/knowledge-core/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Module: knowledge-core
- * Layer: facade
- * @deprecated knowledge-core has been superseded by core/wiki-core.
- * This file re-exports from wiki-core for backward compatibility.
- * Migrate all imports to @/core/wiki-core.
- */
-export * from '../wiki-core'
