5216def66eb9e07765fc3871eb488c29766bba60
122sp7 <122sp7@gmail.com>
2026-04-10 06:04:08 +0800

    feat(authoring): implement server actions for articles and categories; add query functions for Firebase integration

--- modules/notion/subdomains/authoring/interfaces/_actions/index.ts
+++ modules/notion/subdomains/authoring/interfaces/_actions/index.ts
@@ -1,4 +1,19 @@
 // TODO: export server actions: createArticle, updateArticle, publishArticle, archiveArticle
 // TODO: export createCategory, moveCategory
 
-export {};
+export {
+  createArticle,
+  updateArticle,
+  publishArticle,
+  archiveArticle,
+  verifyArticle,
+  requestArticleReview,
+  deleteArticle,
+} from "./article.actions";
+
+export {
+  createCategory,
+  renameCategory,
+  moveCategory,
+  deleteCategory,
+} from "./category.actions";
