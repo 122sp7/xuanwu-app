fec709b9099e99bcd3cbb47bdf9129fe69d6d450
122sp7 <122sp7@gmail.com>
2026-04-10 06:30:23 +0800

    feat: add automation use cases and repository interface for database automation

--- modules/notion/subdomains/database/application/use-cases/index.ts
+++ modules/notion/subdomains/database/application/use-cases/index.ts
@@ -1,6 +1,4 @@
 export { CreateDatabaseUseCase, UpdateDatabaseUseCase, AddFieldUseCase, ArchiveDatabaseUseCase, GetDatabaseUseCase, ListDatabasesUseCase } from "./DatabaseUseCases";
 export { CreateRecordUseCase, UpdateRecordUseCase, DeleteRecordUseCase, ListRecordsUseCase } from "./RecordUseCases";
 export { CreateViewUseCase, UpdateViewUseCase, DeleteViewUseCase, ListViewsUseCase } from "./ViewUseCases";
-// TODO: export LinkArticleToRecordUseCase (Open Host Service for authoring)
-
-export {};
+export { CreateAutomationUseCase, UpdateAutomationUseCase, DeleteAutomationUseCase, ListAutomationsUseCase } from "./AutomationUseCases";
--- modules/notion/subdomains/database/domain/repositories/index.ts
+++ modules/notion/subdomains/database/domain/repositories/index.ts
@@ -1,3 +1,4 @@
 export type { IDatabaseRepository, CreateDatabaseInput, UpdateDatabaseInput, AddFieldInput } from "./IDatabaseRepository";
 export type { IDatabaseRecordRepository, CreateRecordInput, UpdateRecordInput } from "./IDatabaseRecordRepository";
 export type { IViewRepository, CreateViewInput, UpdateViewInput } from "./IViewRepository";
+export type { IAutomationRepository, CreateAutomationInput, UpdateAutomationInput } from "./IAutomationRepository";
