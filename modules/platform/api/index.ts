067a33ca32fbd7c9307c3dd9db20b5a5bfcafb67
122sp7 <122sp7@gmail.com>
2026-04-10 04:32:01 +0800

    fix: update import paths for account use cases and enhance API exports

--- modules/platform/api/index.ts
+++ modules/platform/api/index.ts
@@ -5,3 +5,6 @@
 export * from "./contracts";
 export * from "./facade";
 export * from "../subdomains/identity";
+export * from "../subdomains/account";
+export * from "../subdomains/notification";
+export * from "../subdomains/organization";
