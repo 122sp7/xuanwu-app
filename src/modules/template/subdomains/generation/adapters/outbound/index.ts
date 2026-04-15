936ddff04cb2467cecdcca995a89f4bc0d296fe4
122sp7 <122sp7@gmail.com>
2026-04-15 23:13:32 +0800

    1

---
+++ src/modules/template/subdomains/generation/adapters/outbound/index.ts
@@ -0,0 +1,4 @@
+export { FirestoreGenerationRepository } from './firestore/FirestoreGenerationRepository';
+export type { GeneratedTemplateDoc } from './firestore/FirestoreGenerationRepository';
+export { GenerationMapper } from './firestore/FirestoreGenerationRepository';
+export { AiGenerationAdapter } from './ai/AiGenerationAdapter';
